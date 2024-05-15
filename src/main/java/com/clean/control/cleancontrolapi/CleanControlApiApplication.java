39bb885498558dc2e17d043bb3e9c896572a3828
Gabriel Yukio <126337393+GabrielYukioMC@users.noreply.github.com>
2024-05-15 19:59:17 -0300

    base do projeto

---
+++ src/main/java/com/clean/control/cleancontrolapi/CleanControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.clean.control.cleancontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clean/control/cleancontrolapi/CleanControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clean.control.cleancontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
